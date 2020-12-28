f315a29980bf082c4a5bf31050a80e24b7527980
Dilshod <dilshod20017@git.com>
2020-12-28 17:19:55 -0500

    Dependency container

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Infra.Buss;
+using MicroRabot.Domain.Core.Buss;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //domain 
+            service.AddTransient<IEventBuss, RabbitMQBuss>();
+        }
+    }
+}
