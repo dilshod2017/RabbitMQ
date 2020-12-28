fd7d2531a142bd013301effb1e4835a84249f98d
Dilshod <dilshod20017@git.com>
2020-12-28 18:12:10 -0500

    Customer API and renaming

---
+++ Customer.APP/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Customer.APP
+{
+    public class Class1
+    {
+    }
+}
---
+++ Customer.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Customer.Data
+{
+    public class Class1
+    {
+    }
+}
