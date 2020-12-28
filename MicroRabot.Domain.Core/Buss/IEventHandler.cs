b720431096625f0a4e03b1d35b7ca2b11f0b9b14
Dilshod <dilshod20017@git.com>
2020-12-28 15:48:26 -0500

    Message,Event,Command

---
+++ MicroRabot.Domain.Core/Buss/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabot.Domain.Core.Events;
+
+namespace MicroRabot.Domain.Core.Buss
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+    public interface IEventHandler { }
+}
