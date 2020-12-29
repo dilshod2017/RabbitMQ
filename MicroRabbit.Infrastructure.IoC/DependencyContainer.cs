1ed6d67033e42f7f78f94e1746a1fc159993bf56
Dilshod <dilshod20017@git.com>
2020-12-29 17:37:43 -0500

    Admin Microservice

---
+++ MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //domain bus
+            services?.AddTransient<IEventBus, RabbitMQBuss>();
+
+        }
+    }
+}
